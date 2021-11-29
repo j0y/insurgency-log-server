b48caa281ecb372d5059ad64926eccec3b70d155
j0y <12984600+j0y@users.noreply.github.com>
2021-11-29 22:46:14 +0700

    create files per ip

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	insurgencylog "my.com/insurgency-log"
 	"net"
@@ -10,61 +9,64 @@ import (
 	"time"
 )
 
+var fileWriters = make(map[string]*os.File)
+
 func main() {
 	ServerConn, _ := net.ListenUDP("udp", &net.UDPAddr{IP: []byte{192, 168, 33, 1}, Port: 10001, Zone: ""})
 	defer ServerConn.Close()
 
 	buf := make([]byte, 1024)
 
-	t := time.Now().Unix()
-	timestamp := strconv.FormatInt(t, 10)
-
-	f, err := os.Create(timestamp + ".log")
-	if err != nil {
-		panic(err)
-	}
-
-	datawriter := bufio.NewWriter(f)
-
 	for {
 		n, addr, _ := ServerConn.ReadFromUDP(buf)
-		text := string(buf[5:n])
-
-		message, err := insurgencylog.Parse(text)
-		if err != nil {
-			fmt.Print(err.Error() + ": " + text)
+		if n < 6 {
 			continue
 		}
+		text := string(buf[5:n])
 
-		if message.GetType() == insurgencylog.LoadingMapType {
-			mes, ok := message.(insurgencylog.LoadingMap)
-			if !ok {
-				continue
-			}
+		write(addr.IP.String(), text)
+	}
+}
 
-			err := datawriter.Flush()
-			if err != nil {
-				fmt.Print(err.Error())
-			}
-			err = f.Close()
-			if err != nil {
-				fmt.Print(err.Error())
-			}
+func write(ip string, text string) {
+	message, err := insurgencylog.Parse(text)
+	if err != nil {
+		fmt.Print(err.Error() + ": " + text)
+		return
+	}
 
-			eventTime := mes.Time.Unix()
-			f, err = os.Create(addr.IP.String() + "_" + strconv.FormatInt(eventTime, 10) + "_" + mes.Map + ".log")
-			if err != nil {
-				datawriter.Flush()
-				f.Close()
-				panic(err)
-			}
+	if _, ok := fileWriters[ip]; !ok {
+		t := time.Now().Unix()
+		timestamp := strconv.FormatInt(t, 10)
 
-			datawriter = bufio.NewWriter(f)
+		fileWriters[ip], err = os.Create(ip + "_" + timestamp + ".log")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if message.GetType() == insurgencylog.LoadingMapType {
+		mes, ok := message.(insurgencylog.LoadingMap)
+		if !ok {
+			return
 		}
 
-		_, err = datawriter.WriteString(text + "\n")
+		err = fileWriters[ip].Close()
 		if err != nil {
 			fmt.Print(err.Error())
 		}
+
+		eventTime := mes.Time.Unix()
+
+		fileWriters[ip], err = os.Create(ip + "_" + strconv.FormatInt(eventTime, 10) + "_" + mes.Map + ".log")
+		if err != nil {
+			fileWriters[ip].Close()
+			panic(err)
+		}
+	}
+
+	_, err = fileWriters[ip].WriteString(text + "\n")
+	if err != nil {
+		fmt.Print(err.Error())
 	}
 }
